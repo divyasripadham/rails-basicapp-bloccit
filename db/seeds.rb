a92deecdf7ae4070b92a986ccda677c8ca2f77ca
divyasripadham <divya_sri16@yahoo.com>
2015-10-07 09:27:34 -0400

    Assignment 35 completed, Bloccit CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,18 @@
 include RandomData
 
 50.times do
+  Advertisement.create!(
+    title: RandomData.random_sentence,
+    copy: RandomData.random_paragraph,
+    price: 100
+  )
+end
+
+puts "Seed finished"
+puts "#{Advertisement.count} advertisements created"
+
+=begin
+50.times do
   Post.create!(
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -18,3 +30,5 @@ end
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+
+=end
