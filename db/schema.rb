631589a6be9e38f5debc0eeaa3d781534992c1a8
divyasripadham <divya_sri16@yahoo.com>
2015-10-23 22:40:15 -0400

    Checkpoint 43 completed, Bloccit Voting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022203919) do
+ActiveRecord::Schema.define(version: 20151024022721) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -56,10 +56,26 @@ ActiveRecord::Schema.define(version: 20151022203919) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "ratingjunctions", force: :cascade do |t|
+    t.integer  "rating_id"
+    t.integer  "topic_id"
+    t.integer  "post_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "ratingjunctions", ["post_id"], name: "index_ratingjunctions_on_post_id"
+  add_index "ratingjunctions", ["rateable_type", "rateable_id"], name: "index_ratingjunctions_on_rateable_type_and_rateable_id"
+  add_index "ratingjunctions", ["rating_id"], name: "index_ratingjunctions_on_rating_id"
+  add_index "ratingjunctions", ["topic_id"], name: "index_ratingjunctions_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
@@ -77,5 +93,16 @@ ActiveRecord::Schema.define(version: 20151022203919) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
