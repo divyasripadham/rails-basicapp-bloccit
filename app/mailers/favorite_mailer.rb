9b273ca077e7a72a7ab707abccdc6eaf10168b84
divyasripadham <divya_sri16@yahoo.com>
2015-11-09 09:42:07 -0500

    Email changes, Checkpoint 44 Bloccit Favorites

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,5 +1,5 @@
 class FavoriteMailer < ApplicationMailer
-  default from: "divya_sri16@yahoo.com"
+  default from: "divya_sri16@example.com"
 
   def new_comment(user, post, comment)
     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
